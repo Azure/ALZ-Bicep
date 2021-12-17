ba405bcf9c711fe2e84696845e976b135669b91c
Troy Ault <aultt@hotmail.com>
2021-12-17 14:35:13 -0500

    adding missing modules from Registry example

---
+++ docs/scripts/createRGandcallBicep.ps1
@@ -0,0 +1,29 @@
+
+$azureResourceGroup='rsg-private-bicep-registry'
+$azureLocation='EastUs'
+
+#Create resource group
+New-AzResourceGroup -Name $azureResourceGroup -Location $azureLocation
+
+#Deploy Container Registry into Resource Group
+$deploymentOutput=New-AzResourceGroupDeployment  -TemplateFile infra-as-code/bicep/CRML/containerRegistry/containerRegistry.bicep -ResourceGroupName $azureResourceGroup -name deployACR
+
+#Query the Deployment to get the login server to pass.
+#https://docs.microsoft.com/en-us/cli/azure/query-azure-cli#get-a-single-value
+$azureContainerRegistryName=$deploymentOutput.Outputs.outLoginServer.Value
+
+#Leverage Powershell too loop through all bicep modules within the repository
+#convert the filename to lower case as Azure Container Registry doesnt support Camelcase
+#Leverage az bicep to publish module to Azure Container Registry created above
+$files = $(Get-ChildItem("$pwd/infra-as-code/bicep/modules/*/*.bicep"))
+
+foreach ($file in $files)
+{
+  #Grab the Full Path and Name and Filename only and store as variables
+  $filewithPath=$file.FullName
+  $fileShortName=$file.Name
+  #Grab bicep module name and set to lowercase for Container Registry support
+  $filenamelower = $($fileShortName.Substring(0,$fileShortName.length-6)).toLower()
+  Write-Output "Publishing $filewithPath to ACR: $azureContainerRegistryName"
+  az bicep publish --file "$filewithPath" --target "br:$azureContainerRegistryName/bicep/modules/$($filenamelower):V1"
+}
---
+++ docs/scripts/createRGandcallBicep.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+azureResourceGroup='rsg-private-bicep-registry'
+azureLocation='EastUs'
+
+#Create resource group
+az group create --name $azureResourceGroup --location $azureLocation
+
+#Deploy Container Registry into Resource Group
+az deployment group create --template-file infra-as-code/bicep/CRML/containerRegistry/containerRegistry.bicep  --resource-group $azureResourceGroup --name deployACR
+
+#Query the Deployment to get the login server to pass.
+#https://docs.microsoft.com/en-us/cli/azure/query-azure-cli#get-a-single-value
+azureContainerRegistryName=$(az deployment group show -n deployACR -g $azureResourceGroup --query properties.outputs.outLoginServer.value -o tsv)
+
+#Leverage Bash utilities too loop through all bicep modules within the repository
+#convert the filename to lower case as Azure Container Registry doesnt support Camelcase
+#Leverage az bicep to publish module to Azure Container Registry created above
+for file in infra-as-code/bicep/modules/*/*.bicep
+do
+  f=$(echo "${file##*/}");
+  filename=$(echo $f| cut  -d'.' -f 1| tr '[:upper:]' '[:lower:]')
+  echo "Publishing $filename.bicep to ACR: $azureContainerRegistryName"
+  az bicep publish --file $file --target "br:$azureContainerRegistryName/bicep/modules/$filename:V1"
+done       
