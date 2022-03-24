afad38efd0d076c8402a45e9ad0a470d624017a1
Jan Faurskov <22591930+jfaurskov@users.noreply.github.com>
2022-03-24 10:42:35 +0100

    Swap individual policy assignments to `alzDefaultPolicyAssignments` module in E2E tests (#183)
    * Update file to new path
    
    * Update tests/pipelines/bicep-build-to-validate.yml
    
    Co-authored-by: Jack Tracey <41163455+jtracey93@users.noreply.github.com>
    
    * Update tests/pipelines/bicep-build-to-validate.yml
    
    Co-authored-by: Jack Tracey <41163455+jtracey93@users.noreply.github.com>
    
    * Add script to loop retries
    
    * decrease wait time for script
    
    * add throw on threshold met
    
    * fix unused param
    
    * Update .github/scripts/Set-AlzDefaultPolicyAssignment.ps1
    
    Co-authored-by: Jack Tracey <41163455+jtracey93@users.noreply.github.com>
    
    * Update .github/scripts/Set-AlzDefaultPolicyAssignment.ps1
    
    Co-authored-by: Jack Tracey <41163455+jtracey93@users.noreply.github.com>
    
    * Update tests/pipelines/bicep-build-to-validate.yml
    
    Co-authored-by: Jack Tracey <41163455+jtracey93@users.noreply.github.com>
    
    * Include policyAssignmentManagementGroup.bicep
    
    * fix displayname on pwsh task
    
    * trigger pipeline
    
    * handle string
    
    * remove quotes around subscriptionId
    
    * Include quotes in array
    
    * remove E2E trigger
    
    * re-include trigger
    
    * Remove trigger again
    
    Co-authored-by: Jack Tracey <41163455+jtracey93@users.noreply.github.com>

---
+++ .github/scripts/Set-AlzDefaultPolicyAssignment.ps1
@@ -0,0 +1,32 @@
+
+param (
+    #Added this back into parameters as error occurs if multiple tenants are found when using Get-AzTenant
+    [Parameter(Mandatory = $true)] [string] $ManagementGroupId,
+    [Parameter(Mandatory = $true)] [string] $parLocation,
+    [Parameter(Mandatory = $true)] [string] $templateFile,
+    [Parameter(Mandatory = $true)] [string] $parameterFile,
+    [Parameter(Mandatory = $true)] [string] $parTopLevelManagementGroupPrefix,
+    [Parameter(Mandatory = $true)] [string] $parLogAnalyticsWorkSpaceAndAutomationAccountLocation,
+    [Parameter(Mandatory = $true)] [string] $parLogAnalyticsWorkspaceResourceID,
+    [Parameter(Mandatory = $true)] [string] $parDdosProtectionPlanId
+)
+$state = 'fail'
+$i = 0
+$err.clear
+while ($i -lt 4 -and $state -eq 'fail') {
+    $ErrorActionPreference = "Stop"
+    Try {
+        New-AzManagementGroupDeployment -Managementgroupid $ManagementGroupId -Location $parLocation -TemplateFile $templateFile -TemplateParameterFile $parameterFile -parTopLevelManagementGroupPrefix $parTopLevelManagementGroupPrefix -parLogAnalyticsWorkSpaceAndAutomationAccountLocation $parLogAnalyticsWorkSpaceAndAutomationAccountLocation -parLogAnalyticsWorkspaceResourceID $parLogAnalyticsWorkspaceResourceID -parDdosProtectionPlanId $parDdosProtectionPlanId
+        $state = 'success'
+    }
+    Catch {
+        $i++
+        Write-Output "ALZ Default Policy Assignments module failed to deploy with $error"
+        Write-Output "Iteration number $i"
+        Write-Output "Will retry in 30 seconds"
+        Start-Sleep -Seconds 30
+    }
+}
+If ($state -eq 'fail') {
+    Throw "ALZ Default Policy Assignments module failed to deploy after $i attempts"
+}
