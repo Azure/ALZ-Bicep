3b2acfc6ff35dd49dbbdaafdbe538d9890986672
Jan Faurskov <22591930+jfaurskov@users.noreply.github.com>
2022-07-12 15:16:15 +0200

    linter fixes

--- .github/azFunction/AzFunctionCode/cancelSubscriptions/run.ps1
+++ .github/azFunction/AzFunctionCode/cancelSubscriptions/run.ps1
@@ -1,10 +1,10 @@
 # Input bindings are passed in via param block.
 param($QueueItem, $TriggerMetadata)
 # Write out the queue message and insertion time to the information log.
-Write-Host "PowerShell queue trigger function processed work item: $QueueItem"
+Write-Output "PowerShell queue trigger function processed work item: $QueueItem"
 $subscriptionId = $QueueItem.Body.subscriptionId
 $subscriptionName = $QueueItem.Body.subscriptionName
-Write-Host "Subscription to be canceled is $subscriptionName with id: $subscriptionId"
+Write-Output "Subscription to be canceled is $subscriptionName with id: $subscriptionId"
 $cancelUri = "https://management.azure.com/subscriptions/$($subscriptionId)/providers/Microsoft.Subscription/cancel?api-version=2020-09-01"
 $token = (Get-AzAccessToken).Token | ConvertTo-SecureString -AsPlainText -Force
 Invoke-RestMethod -Method Post -ContentType "application/json" -Authentication Bearer -Token $token -Uri $cancelUri
@@ -15,5 +15,5 @@ $body = @{
 Push-OutputBinding -Name canceledSubscriptions -Value ([HttpResponseContext]@{
         Body = $body
     })
-Write-Host "Queue item insertion time: $($TriggerMetadata.InsertionTime)"
+Write-Output "Queue item insertion time: $($TriggerMetadata.InsertionTime)"
 
--- .github/azFunction/AzFunctionCode/getPullRequests/run.ps1
+++ .github/azFunction/AzFunctionCode/getPullRequests/run.ps1
@@ -4,14 +4,13 @@ param($QueueItem, $TriggerMetadata)
 Write-Output "PowerShell queue trigger function processed work item: $QueueItem"
 Write-Output "Queue item insertion time: $($TriggerMetadata.InsertionTime)"
 $perPageCount = 0
-#fixme include call to get all closed pull requests, i.e. get all pull requests with state closed or similar, for each push to binding
 $closedPrs = Invoke-RestMethod -Method Get -Uri "https://api.github.com/repos/Azure/ALZ-Bicep/pulls?per_page=$perPageCount&state=closed&page=1"
 $closedPrs | Select-Object -unique -Property title, number, state | ForEach-Object {
     $body = @{
         prTitle  = $PSItem.title
         prNumber = $PSItem.number
         prState  = $PSItem.state
-    } 
+    }
     Push-OutputBinding -Name pullRequests -Value ([HttpResponseContext]@{
             Body = $body
         })
