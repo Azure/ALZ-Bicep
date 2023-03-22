2fa78fafcf4d724a997dfce4d3673bb5d76c965f
Zach Trocinski <ztrocinski@outlook.com>
2023-03-21 22:36:03 -0500

    Added initial accelerator pipelines and scripts

---
+++ accelerator/pipeline-scripts/Deploy-LoggingAndSentinel.ps1
@@ -0,0 +1,29 @@
+param (
+  [Parameter()]
+  [String]$Location = "$($env:LOCATION)",
+
+  [Parameter()]
+  [String]$ManagementSubscriptionId = "$($env:MANAGEMENT_SUBSCRIPTION_ID)",
+  
+  [Parameter()]
+  [String]$TopLevelMGPrefix = "$($env:TOP_LEVEL_MG_PREFIX)",
+
+  [Parameter()]
+  [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\logging\logging.bicep",
+
+  [Parameter()]
+  [String]$TemplateParameterFile = "config\custom-parameters\logging.parameters.all.json"
+)
+
+# Parameters necessary for deployment
+$inputObject = @{
+  DeploymentName        = 'alz-LoggingDeploy-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
+  ResourceGroupName     = "rg-alz-logging-001"
+  TemplateFile          = $TemplateFile
+  TemplateParameterFile = $TemplateParameterFile
+  Verbose               = $true
+}
+
+Select-AzSubscription -SubscriptionId $ManagementSubscriptionId
+
+New-AzResourceGroupDeployment @inputObject
