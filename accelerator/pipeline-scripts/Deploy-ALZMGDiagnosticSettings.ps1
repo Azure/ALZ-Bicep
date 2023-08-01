ad6fbf6d9eb3b70b3b67ed21ac2af82ec19626f4
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-08-01 17:15:57 +0300

    ALZ-Bicep-Accelerator Register missing resource provider (#595)

--- accelerator/pipeline-scripts/Deploy-ALZMGDiagnosticSettings.ps1
+++ accelerator/pipeline-scripts/Deploy-ALZMGDiagnosticSettings.ps1
@@ -6,6 +6,9 @@ param (
   [String]$TopLevelMGPrefix = "$($env:TOP_LEVEL_MG_PREFIX)",
 
   [Parameter()]
+  [String]$ManagementSubscriptionId = "$($env:MANAGEMENT_SUBSCRIPTION_ID)",
+
+  [Parameter()]
   [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\orchestration\mgDiagSettingsAll\mgDiagSettingsAll.bicep",
 
   [Parameter()]
@@ -24,6 +27,33 @@ $inputObject = @{
   TemplateParameterFile = $TemplateParameterFile
   WhatIf                = $WhatIfEnabled
   Verbose               = $true
+}
+
+# Registering 'Microsoft.Insights' resource provider on the Management subscription
+Select-AzSubscription -SubscriptionId $ManagementSubscriptionId
+
+$providers = @('Microsoft.insights')
+
+foreach ($provider in $providers ){
+  $iterationCount = 0
+  $maxIterations = 30
+  $providerStatus= (Get-AzResourceProvider -ListAvailable | Where-Object ProviderNamespace -eq $provider).registrationState
+  if($providerStatus -ne 'Registered'){
+    Write-Output "`n Registering the '$provider' provider"
+    Register-AzResourceProvider -ProviderNamespace $provider
+    do {
+      $providerStatus= (Get-AzResourceProvider -ListAvailable | Where-Object ProviderNamespace -eq $provider).registrationState
+      $iterationCount++
+      Write-Output "Waiting for the '$provider' provider registration to complete....waiting 10 seconds"
+      Start-Sleep -Seconds 10
+    } until ($providerStatus -eq 'Registered' -and $iterationCount -ne $maxIterations)
+    if($iterationCount -ne $maxIterations){
+      Write-Output "`n The '$provider' has been registered successfully"
+    }
+    else{
+      Write-Output "`n The '$provider' has not been registered successfully"
+    }
+  }
 }
 
 New-AzManagementGroupDeployment @inputObject
