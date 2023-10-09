e454f06bb02c6d781369e215b0e8561670932070
Jack Tracey <jack@jacktracey.co.uk>
2023-10-09 19:40:18 +0100

    add accelerator flex

---
+++ accelerator/pipeline-scripts/Deploy-ALZManagementGroupsScopeEscape.ps1
@@ -0,0 +1,47 @@
+param (
+  [Parameter()]
+  [String]$NonRootParentManagementGroupId = "$($env:NONROOTPARENTMANAGEMENTGROUPID)",
+
+  [Parameter()]
+  [String]$Location = "$($env:LOCATION)",
+
+  [Parameter()]
+  [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\managementGroups\",
+
+  [Parameter()]
+  [String]$TemplateParameterFile = "config\custom-parameters\managementGroups.parameters.all.json",
+
+  [Parameter()]
+  [Boolean]$WhatIfEnabled = [System.Convert]::ToBoolean($($env:IS_PULL_REQUEST))
+)
+
+# Parameters necessary for deployment
+
+if ($NonRootParentManagementGroupId -eq '') {
+  $inputObject = @{
+    DeploymentName        = 'alz-MGDeployment-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
+    Location              = $Location
+    TemplateFile          = $TemplateFile + "managementGroups.bicep"
+    TemplateParameterFile = $TemplateParameterFile
+    WhatIf                = $WhatIfEnabled
+    Verbose               = $true
+  }
+
+  New-AzTenantDeployment @inputObject
+}
+
+if ($NonRootParentManagementGroupId -ne '') {
+  $inputObject = @{
+    ManagementGroupId     = $NonRootParentManagementGroupId
+    DeploymentName        = 'alz-MGDeployment-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
+    Location              = $Location
+    TemplateFile          = $TemplateFile + "managementGroupsScopeEscape.bicep"
+    TemplateParameterFile = $TemplateParameterFile
+    WhatIf                = $WhatIfEnabled
+    Verbose               = $true
+  }
+
+  New-AzManagementGroupDeployment @inputObject
+}
+
+
