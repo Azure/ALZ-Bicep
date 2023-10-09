b5f77ad83b7b5dcade5bc98ef189e047be71f702
Jack Tracey <jack@jacktracey.co.uk>
2023-10-09 21:15:04 +0100

    remove file not needed

--- accelerator/pipeline-scripts/Deploy-ALZManagementGroups.ps1
+++ accelerator/pipeline-scripts/Deploy-ALZManagementGroups.ps1
@@ -1,9 +1,12 @@
 param (
   [Parameter()]
+  [String]$NonRootParentManagementGroupId = "$($env:NONROOTPARENTMANAGEMENTGROUPID)",
+
+  [Parameter()]
   [String]$Location = "$($env:LOCATION)",
 
   [Parameter()]
-  [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\managementGroups\managementGroups.bicep",
+  [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\modules\managementGroups\",
 
   [Parameter()]
   [String]$TemplateParameterFile = "config\custom-parameters\managementGroups.parameters.all.json",
@@ -13,13 +16,30 @@ param (
 )
 
 # Parameters necessary for deployment
-$inputObject = @{
-  DeploymentName        = 'alz-MGDeployment-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
-  Location              = $Location
-  TemplateFile          = $TemplateFile
-  TemplateParameterFile = $TemplateParameterFile
-  WhatIf                = $WhatIfEnabled
-  Verbose               = $true
+
+if ($NonRootParentManagementGroupId -eq '') {
+  $inputObject = @{
+    DeploymentName        = 'alz-MGDeployment-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
+    Location              = $Location
+    TemplateFile          = $TemplateFile + "managementGroups.bicep"
+    TemplateParameterFile = $TemplateParameterFile
+    WhatIf                = $WhatIfEnabled
+    Verbose               = $true
+  }
+
+  New-AzTenantDeployment @inputObject
 }
 
-New-AzTenantDeployment @inputObject
+if ($NonRootParentManagementGroupId -ne '') {
+  $inputObject = @{
+    ManagementGroupId     = $NonRootParentManagementGroupId
+    DeploymentName        = 'alz-MGDeployment-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
+    Location              = $Location
+    TemplateFile          = $TemplateFile + "managementGroupsScopeEscape.bicep"
+    TemplateParameterFile = $TemplateParameterFile
+    WhatIf                = $WhatIfEnabled
+    Verbose               = $true
+  }
+
+  New-AzManagementGroupDeployment @inputObject
+}
